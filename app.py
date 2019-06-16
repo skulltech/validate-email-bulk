a6768a2ab6a4f0fe52ff041fe06e829b394cbd42
Sumit Ghosh <sumit@sumit-ghosh.com>
2019-06-16 16:06:28 +0530

    Complete the whole things, except maybe some JS stuff to be executed on completion.

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import csv
+import uuid
+from datetime import datetime, timedelta
 from io import StringIO
 
 import redis
 from flask import Flask, render_template, request, jsonify, send_file
 from rq import Queue
-from walrus import *
+from rq_scheduler import Scheduler
+from walrus import Walrus
 
 from models import push, pull, export
 from validate import validate
@@ -14,13 +17,50 @@ app = Flask(__name__)
 conn = redis.from_url('redis://127.0.0.1:6379')
 q = Queue(connection=conn)
 
+high_queue = Queue('high', connection=conn)
+scheduler = Scheduler(queue=high_queue, connection=conn)
+
 wr = Walrus(host='localhost', port=6379, db=0)
 
 
-def task(email, submission=None):
+def remove_submission(submission):
+    for job in wr.List(submission + '_tasks'):
+        job = q.fetch_job(job.decode('UTF-8'))
+        if job:
+            job.cancel()
+    wr.Set('submission_ids').remove(submission)
+
+
+def cron():
+    for submission in wr.Set('submission_ids'):
+        submission = submission.decode('UTF-8')
+        then = datetime.fromisoformat(wr[submission + '_polled'].decode('UTF-8'))
+        if datetime.utcnow() - then > timedelta(seconds=10):
+            print(f'[*] Removing submission: {submission}')
+            remove_submission(submission)
+
+
+@app.before_first_request
+def schedule_cleaner():
+    prev_jobs = scheduler.get_jobs()
+    for j in prev_jobs:
+        scheduler.cancel(j)
+
+    scheduler.schedule(
+        scheduled_time=datetime.utcnow(),
+        func=cron,
+        args=[],
+        interval=10
+    )
+
+
+def task(email, submission=None, deep=False):
     record = pull(email)
     if record:
         if record['color'] in ['green', 'red']:
+            if submission:
+                c = wr.counter(submission + '_completed')
+                c.incr()
             return record
     response = validate(email)
     created = push(response)
@@ -44,9 +84,10 @@ def index():
     return render_template('index.html')
 
 
-def __process(emails):
+def __process(emails, deep=False):
     submission = uuid.uuid4().hex
     print(f'[*] New submission: {submission}. Registering...')
+    wr.Set('submission_ids').add(submission)
     tasks = enqueue_emails(emails=emails, submission=submission)
     l = wr.List(submission + '_tasks')
     try:
@@ -62,6 +103,7 @@ def __process(emails):
             l.append(i)
     wr.counter(submission + '_completed')
     print(f'[*] Registered submission: {submission}')
+    wr[submission + '_polled'] = datetime.utcnow().isoformat()
     return submission
 
 
@@ -70,10 +112,11 @@ def process():
     f = request.files['file']
     if not f:
         return 'No file'
+    deep = request.args.get('deep')
 
     stream = StringIO(f.stream.read().decode('UTF8'), newline=None)
     emails = [line[0] for line in csv.reader(stream) if line[0]]
-    submission = __process(emails)
+    submission = __process(emails, deep)
 
     response = {
         'status': 'success',
@@ -109,6 +152,7 @@ def get_status(submission):
         response['data']['finished'] = finished
         response['data']['failed'] = failed
 
+    wr[submission + '_polled'] = datetime.utcnow().isoformat()
     return jsonify(response)
 
 
---
+++ static/script.js
@@ -0,0 +1,68 @@
+// custom javascript
+
+$(document).ready(() => {
+    console.log('Sanity Check!');
+});
+
+//USAGE: $("#form").serializefiles();
+(function ($) {
+    $.fn.serializefiles = function () {
+        var obj = $(this);
+        /* ADD FILE TO PARAM AJAX */
+        var formData = new FormData();
+        $.each($(obj).find("input[type='file']"), function (i, tag) {
+            $.each($(tag)[0].files, function (i, file) {
+                formData.append(tag.name, file);
+            });
+        });
+        var params = $(obj).serializeArray();
+        $.each(params, function (i, val) {
+            formData.append(val.name, val.value);
+        });
+        return formData;
+    };
+})(jQuery);
+
+$('form').submit(function (event) {
+    var formdata = $(this).serializefiles();
+    console.log(formdata);
+    event.preventDefault();
+
+    var l = Ladda.create(document.querySelector('.ladda-button'));
+    l.start();
+
+    $.ajax(
+        {
+            url: '/process',
+            data: formdata,
+            method: 'POST',
+            contentType: false,
+            processData: false
+        }
+    ).done((res) => {
+        getStatus(res.data.submission_id)
+    }).fail((err) => {
+        console.log(err)
+    })
+});
+
+function getStatus(submission_id) {
+    $.ajax({
+        url: `/status/${submission_id}`,
+        method: 'GET'
+    }).done((res) => {
+        let completed = res.data.completed;
+        let total = res.data.total_tasks;
+        console.log(total, completed);
+
+        var l = Ladda.create(document.querySelector('.ladda-button'));
+        let progress = completed / total;
+        l.setProgress(progress);
+
+        setTimeout(function () {
+            getStatus(res.data.submission_id);
+        }, 1000);
+    }).fail((err) => {
+        console.log(err)
+    })
+}
